fa429727666cada2acd6d2bbf2a742a98833fd1e
ChenYong Tian <1805170243@qq.com>
2018-11-26 21:13:38 +0800

    clear

--- package/vuc-carousel/example/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vucCarousel from '../dist/vuc-carousel'
-
-Vue.use(vucCarousel)
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
--- package/vuc-material/example/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vucMaterial from '../src/main.js'
-
-Vue.use(vucMaterial)
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
